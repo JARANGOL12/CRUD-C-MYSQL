25bbb23411887d3d16cda4e60814952064bc3b6f
soy Jose <brazymoreno11@gmail.com>
2024-05-26 13:37:31 -0500

    mi primer comimit de c# y mysql

---
+++ Sol_Alamcen.Presentacion/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_Alamcen.Presentacion
+{
+    public  class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Puerto;
+        private string Usuario;
+        private string Clase;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "bd_almacen";
+            this.s
+        }
+    }
+}
